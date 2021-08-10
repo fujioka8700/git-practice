72e7d82c69af65b50cb3a1301b461b9ef317b2aa
fujioka8700 <fujioka8700@gmail.com>
2021-08-10 20:55:24 +0900

    綺麗に修正しました。

--- sort.js
+++ sort.js
@@ -1,10 +1,6 @@
 var sortNumber = function (number) {
     number.sort(function (a, b) {
-<<<<<<< HEAD
-
-=======
         if (a === b) {
->>>>>>> 407b26f35e355088471d029c33f6b08fc4f5eedf
             return 0;
         }
         return a < b ? -1 : 1;
